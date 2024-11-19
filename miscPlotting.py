dac451d5851923ef2b6a3726c21bf280331aa8ce
Aadi <122440845+aadi-bh@users.noreply.github.com>
2024-11-19 16:06:00 +0100

    New plots and plotting functions

--- miscPlotting.py
+++ miscPlotting.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 import os
 import datetime
 import socket
+import numpy as np
 import seaborn as sns
 from operator import itemgetter
+import gc
 
 #Set large figure sizes
 plt.rcParams['figure.figsize'] = [12, 8]
@@ -14,6 +16,10 @@ plt.rcParams['animation.html'] = 'html5'
 #plt.rcParams['lines.markersize'] = 6
 plt.rcParams['lines.markeredgewidth'] = 1.5
 #plt.rcParams['savefig.dpi'] = 400
+
+
+def gen_filename(simulator, nx):
+    return os.path.abspath(os.path.join("data", "smooth1d", str(simulator.__name__) + "_" + str(nx) + ".npz"))
 
 def setBwStyles(ax):
     from cycler import cycler
@@ -46,3 +52,25 @@ def save_figure(fig, stem):
         fig.savefig(fig_filename, dpi=300,format='pdf',
                 transparent=True, pad_inches=0.0, facecolor=None, 
                 metadata=metadata)
+
+def plot_solution(simulator, nx, label, **kwargs):
+    datafilename = gen_filename(simulator, nx)
+    
+    #Read the solution
+    with np.load(datafilename) as data:
+        h = data['h']
+        
+    x = np.linspace(0.5, nx-0.5, nx)*100.0/float(nx)
+    y = h[0,:]
+    
+    plt.plot(x, y, label=label, **kwargs)
+    
+    h = None
+    x = None
+    gc.collect() # Force run garbage collection to free up memory
+    
+def plot_comparison(nx, **kwargs):
+    plot_solution(HLL2.HLL2, reference_nx, 'Reference', marker=' ', linestyle='-')
+
+    for i, simulator in enumerate(simulators):
+        plot_solution(simulator, nx, simulator.__name__, **kwargs)
