2f02e4cad5df3614225ba2f38ff991f5d75ea44b
Aadi <122440845+aadi-bh@users.noreply.github.com>
2024-12-06 16:18:18 +0100

    Move common parts out of `simulate.py`

---
+++ common_simulator.py
@@ -0,0 +1,108 @@
+import argparse
+import logging
+import sys
+import os
+import pycuda.driver as cuda
+import atexit
+import numpy as np
+
+from GPUSimulators import Common, LxF, FORCE, HLL, HLL2, KP07, KP07_dimsplit, WAF
+from GPUSimulators import CudaContext
+from GPUSimulators.helpers import InitialConditions
+
+
+def gen_filename(args, nx, ny, prefix=None):
+    if prefix == None:
+        if args.nt == np.inf and args.tf < np.inf:
+            prefix = 'space'
+        elif args.nt < np.inf  and args.tf == np.inf:
+            prefix = 'time'
+        else:
+            raise ValueError("gen_filename unable deduce the purpose of simulation.\n One and only one of args.nt and args.tf must be finite.")
+    directory = prefix + '_data'
+    return os.path.abspath(os.path.join(directory, str(args.ic.__name__), str(args.simulator.__name__) + "_" + str(nx) + "_" + str(ny) + ".npz"))
+
+def init_logger(name, outfile, print_level=20, file_level=10):
+    logger = logging.getLogger(name)
+    logger.setLevel(min(print_level, file_level))
+
+    ch = logging.StreamHandler()
+    ch.setLevel(print_level)
+    logger.addHandler(ch)
+    logger.log(print_level, "Console logger using level %s", logger.level)
+
+    logger.log(file_level, "File logging level %s", file_level)
+
+    fh = logging.FileHandler(outfile)
+    formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s: %(message)s')
+    fh.setFormatter(formatter)
+    fh.setLevel(file_level)
+    logger.addHandler(fh)
+
+    logger.info("Python version %s", sys.version)
+    return logger
+
+def create_cuda_context(name, blocking=False, no_cache=False, use_autotuning=True):
+    context_flags = None
+    if (blocking):
+        context_flags = cuda.ctx_flags.SCHED_BLOCKING_SYNC
+    logger = logging.getLogger(__name__)
+    logger.debug("Creating context")
+    context = CudaContext.CudaContext(context_flags=context_flags, 
+                                     use_cache=not no_cache, 
+                                     autotuning = use_autotuning)
+    def exitfunc():
+        logger.info("Exitfunc: Resetting CUDA context stack")
+        while (cuda.Context.get_current() != None):
+            context = cuda.Context.get_current()
+            logger.info("Popping <%s>", str(context.handle))
+            cuda.Context.pop()
+        logger.debug("===")
+    atexit.register(exitfunc)
+    return context
+    
+class GetSimulator(argparse.Action):
+    simulators = {'LxF': LxF.LxF,
+                  'FORCE': FORCE.FORCE,
+                  'HLL': HLL.HLL,
+                  'HLL2': HLL2.HLL2,
+                  'KP07': KP07.KP07,
+                  'KP07_dimsplit': KP07_dimsplit.KP07_dimsplit,
+                  'WAF': WAF.WAF,
+                  }
+    def __call__(self, _, namespace, values, option_string=None):
+        setattr(namespace, self.dest, self.simulators[values])
+    num_ghost_cells = {'LxF': 1,
+                       'FORCE': 1,
+                       'HLL': 1,
+                       'HLL2': 2,
+                       'KP07': 2,
+                       'KP07_dimsplit': 2,
+                       'WAF': 2}
+
+class GetInitialCondition(argparse.Action):
+    ics = {'bump': {'fn': InitialConditions.bump,
+#                    'tf': 1.0,
+#                    'max_nt': 100, # for now
+                    'test_data_args': {
+                        'width': 100,
+                        'height': 100
+                    }
+                    },
+          'dambreak': {'fn': InitialConditions.dambreak,
+                    'test_data_args': {
+                        'width': 100,
+                        'height': 100,
+                        'damloc' : 0.5,
+                    }
+                  },
+                  'constant': {'fn': InitialConditions.constant,
+                               'test_data_args': {
+                                   'width': 10,
+                                   'height': 10,
+                                   'constant': 1.0}
+                  }
+        }
+    def __call__(self, _, namespace, values, option_string=None):
+        data = self.ics[values]
+        setattr(namespace, self.dest, data['fn'])
--- simulate.py
+++ simulate.py
@@ -21,48 +21,7 @@ from GPUSimulators import Common, LxF, FORCE, HLL, HLL2, KP07, KP07_dimsplit, WA
 from GPUSimulators import CudaContext
 from GPUSimulators.helpers import InitialConditions
 
-def init_logger(name, outfile, print_level=1, file_level=10):
-    logger = logging.getLogger(name)
-    logger.setLevel(min(print_level, file_level))
-
-    ch = logging.StreamHandler()
-    ch.setLevel(print_level)
-    logger.addHandler(ch)
-    logger.log(print_level, "Console logger using level %s", logger.level)
-
-    logger.log(file_level, "File logging level %s", file_level)
-
-    fh = logging.FileHandler(outfile)
-    formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s: %(message)s')
-    fh.setFormatter(formatter)
-    fh.setLevel(file_level)
-    logger.addHandler(fh)
-
-    logger.info("Python version %s", sys.version)
-    return logger
-
-def create_cuda_context(name, blocking=False, no_cache=False, use_autotuning=True):
-    context_flags = None
-    if (blocking):
-        context_flags = cuda.ctx_flags.SCHED_BLOCKING_SYNC
-    logger = logging.getLogger(__name__)
-    logger.debug("Creating context")
-    context = CudaContext.CudaContext(context_flags=context_flags, 
-                                     use_cache=not no_cache, 
-                                     autotuning = use_autotuning)
-    def exitfunc():
-        logger.info("Exitfunc: Resetting CUDA context stack")
-        while (cuda.Context.get_current() != None):
-            context = cuda.Context.get_current()
-            logger.info("Popping <%s>", str(context.handle))
-            cuda.Context.pop()
-        logger.debug("===")
-    atexit.register(exitfunc)
-    return context
-    
-def gen_filename(args, nx, ny):
-    return os.path.abspath(os.path.join("data", str(args.ic.__name__), str(args.simulator.__name__) + "_" + str(nx) + "_" + str(ny) + ".npz"))
-
+from common_simulator import *
 
 def run_benchmark(datafilename, simulator, simulator_args, ic, nx, reference_nx, ny, reference_ny,
                   dt=None, tf=1.0, max_nt=np.inf, force_rerun=False, transpose=False):
@@ -119,51 +78,7 @@ def run_benchmark(datafilename, simulator, simulator_args, ic, nx, reference_nx,
     gc.collect() # Force garbage collection
     return [t, nt, elapsed_time]
 
-class GetSimulator(argparse.Action):
-    simulators = {'LxF': LxF.LxF,
-                  'FORCE': FORCE.FORCE,
-                  'HLL': HLL.HLL,
-                  'HLL2': HLL2.HLL2,
-                  'KP07': KP07.KP07,
-                  'KP07_dimsplit': KP07_dimsplit.KP07_dimsplit,
-                  'WAF': WAF.WAF,
-                  }
-    def __call__(self, _, namespace, values, option_string=None):
-        setattr(namespace, self.dest, self.simulators[values])
-    num_ghost_cells = {'LxF': 1,
-                       'FORCE': 1,
-                       'HLL': 1,
-                       'HLL2': 2,
-                       'KP07': 2,
-                       'KP07_dimsplit': 2,
-                       'WAF': 2}
 
-class GetInitialCondition(argparse.Action):
-    ics = {'bump': {'fn': InitialConditions.bump,
-#                    'tf': 1.0,
-#                    'max_nt': 100, # for now
-                    'test_data_args': {
-                        'width': 100,
-                        'height': 100
-                    }
-                    },
-          'dambreak': {'fn': InitialConditions.dambreak,
-                    'test_data_args': {
-                        'width': 100,
-                        'height': 100,
-                        'damloc' : 0.5,
-                    }
-                  },
-                  'constant': {'fn': InitialConditions.constant,
-                               'test_data_args': {
-                                   'width': 10,
-                                   'height': 10,
-                                   'constant': 1.0}
-                  }
-        }
-    def __call__(self, _, namespace, values, option_string=None):
-        data = self.ics[values]
-        setattr(namespace, self.dest, data['fn'])
             
 
 if __name__ == "__main__":
@@ -174,31 +89,21 @@ if __name__ == "__main__":
     parser.add_argument('--nx', type=int, default=128)
     parser.add_argument('--ref-nx', type=int, default=8192)
     parser.add_argument('--ny', type=int, default=2)
-    parser.add_argument('--ref-ny', type=int, default=None)
+    parser.add_argument('--ref-ny', type=int, default=2)
     parser.add_argument('--force-rerun', action='store_true', default=True, help="Rerun simulation even if corresponding datafile exists")
     parser.add_argument('--transpose', action='store_true', default=False, help="run the transposed initial condition")
     parser.add_argument('--logfile', default='gpusimulator', help="Name of log file")
-    parser.add_argument('--tf', type=float, default=None)
-    parser.add_argument('--nt', type=int, default=None)
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--tf', type=float, default=np.inf)
+    group.add_argument('--nt', type=int, default=np.inf)
+    args = parser.parse_args()
 
     logger = init_logger(__name__, 'gpusimulator.log')
     logger = logging.getLogger(__name__)
 
-#    H_REF = 0.5
-#    H_AMP = 0.1
-#    U_REF = 0.0
-#    U_AMP = 0.1
-    args = parser.parse_args()
-    if args.tf != None:
-        args.nt = np.inf
-    elif args.nt != None:
-        args.tf = np.inf
-    else:
-        raise ValueError("Please specify at least one of --tf or --nt")
     logger.info("Arguments: " + str(args))
 
-    ctx = create_cuda_context('nonamestoday')
-
+    ctx = create_cuda_context('benchmark_context')
     sim_args = {
     'context': ctx,
     'g': 9.81,
@@ -212,13 +117,8 @@ if __name__ == "__main__":
         'transpose': args.transpose,
         #dt=0.25*0.7*(width/ref_nx)/(u_ref+u_amp + np.sqrt(g*(h_ref+h_amp))),
     }
-    # Make space to store
-    # sim_elapsed_time = 0.0
-    # sim_dt = 0.0
-    # sim_nt = 0.0
-        # Run reference with a low CFL-number. TODO IT DOES NOT!
-        # This should also serve as warmup for now? TODO
-        # warmup!
+    # Run reference with a low CFL-number. TODO IT DOES NOT!
+    # warmup!
     _, _, secs = run_benchmark(datafilename = None,
                                **benchmark_args,
                                nx=min(16, args.nx), reference_nx=min(16, args.ref_nx),
