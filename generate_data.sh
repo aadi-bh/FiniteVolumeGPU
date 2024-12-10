a5812aaacff510659dd52a5acf24ee83d7061304
Aadi <122440845+aadi-bh@users.noreply.github.com>
2024-12-10 10:07:52 +0100

    Add `generate_data.sh`

--- generate_data.sh
+++ generate_data.sh
@@ -2,9 +2,9 @@
 # conda activate ShallowWaterGPU_py310
 
 python batch_simulators.py --sizes-file domain_sizes.csv --tf 1.0 bump
-python batch_simulators.py --sizes-file domain_sizes.csv --nt 100 bump
-python batch_simulators.py --sizes-file domain_sizes.csv --tf 6.0 dambreak
-python batch_simulators.py --sizes-file domain_sizes.csv --nt 100 dambreak
-python batch_simulators.py --sizes-file domain_sizes.csv --tf 1.0 constant
-python batch_simulators.py --sizes-file domain_sizes.csv --nt 100 constant
+python batch_simulators.py --sizes-file domain_sizes.csv --nt 1000 bump
+# python batch_simulators.py --sizes-file domain_sizes.csv --tf 6.0 dambreak
+#python batch_simulators.py --sizes-file domain_sizes.csv --nt 100 dambreak
+#python batch_simulators.py --sizes-file domain_sizes.csv --tf 1.0 constant
+#python batch_simulators.py --sizes-file domain_sizes.csv --nt 100 constant
 
